346629fedbcd2515d15150b8377c2caca3754b2f
Gene Tenorlas <tenorlas@sheridancollege.ca>
2022-09-23 01:28:50 +0000

    Create and add date for assignments

---
+++ .history/migrations/assignments_submissions_20220922212200.sql
@@ -0,0 +1,16 @@
+CREATE TABLE assignments(
+  id SERIAL PRIMARY KEY NOT NULL,
+  name TEXT,
+  content TEXT,
+  day TEXT,
+  chapter INTEGER,
+  duration INTEGER
+);
+
+CREATE TABLE assignment_submissions(
+  id SERIAL PRIMARY KEY NOT NULL,
+  duration INTEGER,
+  submission_date DATE,
+  assignments_id INTEGER REFERENCES assignments(id),
+  students_id INTEGER REFERENCES students(id)
+);
